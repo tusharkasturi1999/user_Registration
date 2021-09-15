21d3b2987960f92255de6ac1606d4094fee5e556
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-15 17:43:02 +0530

    Added: lastName option

--- src/main/java/com/bridgelabz/regex/user_Registration/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/user_Registration/UserRegistration.java
@@ -10,16 +10,23 @@ public class UserRegistration {
 
 	public static final Scanner sc = new Scanner(System.in);
 	public final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+	public final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 
 	// This method checks if the entered first name is valid
 	public boolean validateFirstName(String fname) {
 		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
 		return pattern.matcher(fname).matches();
 	}
+	
+	public boolean validateLastName(String lname) {
+		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+		return pattern.matcher(lname).matches();
+	}
 
 	
 	// This is the main function
 	public static void main(String[] args) {
+		
 		System.out.println("Enter First Name: ");
 		String fname = sc.next();
 		UserRegistration obj = new UserRegistration();
@@ -27,6 +34,14 @@ public class UserRegistration {
 			System.out.println("First Name Valid");
 		} else {
 			System.out.println("First Name Invalid");
+		}
+		
+		System.out.println("\nEnter Last Name: ");
+		String lname = sc.next();
+		if (obj.validateFirstName(lname)) {
+			System.out.println("Last Name Valid");
+		} else {
+			System.out.println("Last Name Invalid");
 		}
 	}
 }

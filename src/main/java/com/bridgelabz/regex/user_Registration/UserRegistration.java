a842425511847eb4b2db81d5f200709dc566065a
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-15 17:46:10 +0530

    Added: phone option

--- src/main/java/com/bridgelabz/regex/user_Registration/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/user_Registration/UserRegistration.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.regex.user_Registration;
+
 import java.util.*;
 import java.util.regex.*;
 
@@ -11,6 +12,7 @@ public class UserRegistration {
 	public final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 	public final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 	public final String EMAIL_PATTERN = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.[a-z]*[.a-z]?$";
+	public final String PHONE_PATTERN = "^[0-9]{2}[0-9]{10}$";
 
 	// This method checks if the entered first name is valid
 	public boolean validateFirstName(String fname) {
@@ -30,12 +32,18 @@ public class UserRegistration {
 		return pattern.matcher(email).matches();
 	}
 
+	// This method checks if the entered phone number is valid
+	public boolean validatePhone(String phone) {
+		Pattern pattern = Pattern.compile(PHONE_PATTERN);
+		return pattern.matcher(phone).matches();
+	}
+
 	// This is the main function
 	public static void main(String[] args) {
 
+		UserRegistration obj = new UserRegistration();
 		System.out.println("Enter First Name: ");
 		String fname = sc.next();
-		UserRegistration obj = new UserRegistration();
 		if (obj.validateFirstName(fname)) {
 			System.out.println("First Name Valid");
 		} else {
@@ -57,6 +65,14 @@ public class UserRegistration {
 			System.out.println("Email Valid");
 		} else {
 			System.out.println("Email Invalid");
+		}
+
+		System.out.println("\nEnter Phone No with Country Code: ");
+		String phone = sc.next();
+		if (obj.validatePhone(phone)) {
+			System.out.println("Phone No. Valid");
+		} else {
+			System.out.println("Phone No. Invalid");
 		}
 	}
 }

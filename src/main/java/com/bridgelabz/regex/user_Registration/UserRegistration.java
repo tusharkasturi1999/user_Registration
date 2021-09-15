d276e23f0c50e730ea5b6f7fc0e7afac28145687
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-15 17:37:57 +0530

    Added: firstName option

---
+++ src/main/java/com/bridgelabz/regex/user_Registration/UserRegistration.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.regex.user_Registration;
+
+import java.util.*;
+import java.util.regex.*;
+
+/**
+ * @author tushar.kasturi_ymedi This is the UserRegistration Class
+ */
+public class UserRegistration {
+
+	public static final Scanner sc = new Scanner(System.in);
+	public final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+
+	// This method checks if the entered first name is valid
+	public boolean validateFirstName(String fname) {
+		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+		return pattern.matcher(fname).matches();
+	}
+
+	// This is the main function
+	public static void main(String[] args) {
+		System.out.println("Enter First Name: ");
+		String fname = sc.next();
+		UserRegistration obj = new UserRegistration();
+		if (obj.validateFirstName(fname)) {
+			System.out.println("First Name Valid");
+		} else {
+			System.out.println("First Name Invalid");
+		}
+	}
+}
